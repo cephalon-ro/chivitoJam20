e064132d3b0da98786b95a5ea81e9b7253e48625
eluzinatoRRRR <rodrigosclavi7395@gmail.com>
2020-02-01 23:06:39 -0300

    fixed scene implemented warp on scene 1

--- GGJ20/Assets/Scripts/Satellite.cs
+++ GGJ20/Assets/Scripts/Satellite.cs
@@ -6,25 +6,34 @@ namespace Assets.Scripts
     {
         public string codename;
         public Slider hpSlider;
-        public Text satText;
+        public TextMesh satText;
         public float hp;
         public float maxHp;
-
-         void Start()
+        RectTransform rt;
+        void Start()
         {
+            satText.gameObject.SetActive(false);
+            satText.transform.position = new Vector3(transform.position.x, (transform.position.y + 8), transform.position.z);
+            rt = hpSlider.GetComponent<RectTransform>();
+            rt.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            hpSlider.transform.position = new Vector3(transform.position.x, (transform.position.y + 5), transform.position.z);
             hp = maxHp;
             hpSlider.maxValue = maxHp;
             hpSlider.value = hp;
-            satText.fontSize = 20;
             satText.text = codename;
         }
         void Update()
         {
-           hp -= 1 * Time.deltaTime;
-           hpSlider.value = hp;
+            hp -= 1 * Time.deltaTime;
+            hpSlider.value = hp;
+
+            if (hp <= 0)
+            {
+                hpSlider.gameObject.SetActive(false);
+            }
         }
     }
-  
-    
+
+
 
 }
---
+++ GGJ20/Assets/Scripts/faceCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class faceCamera : MonoBehaviour
+{
+    public Camera m_camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_camera = FindObjectOfType<CameraSingleton>().cam;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + m_camera.transform.rotation * Vector3.forward,
+    m_camera.transform.rotation * Vector3.up);
+    }
+}
