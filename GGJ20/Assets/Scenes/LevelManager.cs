9467ff303e96e366296a4b44de383c6428065ee8
ADMINRG-BV781OL\Zera <guillegar17@gmail.com>
2020-02-02 00:52:05 -0300

    Main menu works god damnit

--- GGJ20/Assets/Scenes/LevelManager.cs
+++ GGJ20/Assets/Scenes/LevelManager.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+
+    public float mouseX;
+    public float mouseY;
+    public float mouseZ;
+    public bool mouseClick;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +21,16 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mouseX = Input.mousePosition.x;
+        mouseY = Input.mousePosition.y;
+        mouseZ = Input.mousePosition.z;
+        Input.GetKey(KeyCode.Mouse0);
     }
 
 
     public void StartScene() {
-        //Debug.Log("a");
-        SceneManager.LoadScene("SampleScene");
+        Debug.Log("clickeado");
+        //SceneManager.LoadScene("SampleScene");
     }
 
     public void CreditsScene() {
